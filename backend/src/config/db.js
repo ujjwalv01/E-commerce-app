2309dc292cd9c607c537cedee8653f167521fb20
Ujjwal Verma <ujjwalv01@gmail.com>
2026-03-31 16:02:42 +0530

    Initial clean commit

---
+++ backend/src/config/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { ENV } from "./env.js";
+
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(
+  "mongodb+srv://ujjwalverma010305_db_user:RuWQnyhQqL0jE8LG@cluster0.kbzprpb.mongodb.net/ecommerce?retryWrites=true&w=majority&tls=true"
+);
+    console.log(`✅ Connected to MONGODB: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(" MONGODB connection error");
+    process.exit(1); // exit code 1 means failure, 0 means success
+  }
+};
---
+++ mobile/app/_layout.tsx
@@ -0,0 +1,71 @@
+import { Stack } from "expo-router";
+import "../global.css";
+import { MutationCache, QueryCache, QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
+import * as Sentry from "@sentry/react-native";
+import { StripeProvider } from "@stripe/stripe-react-native";
+
+Sentry.init({
+  dsn: "https://fb6731b90610cc08333e6c16ffac5724@o4509813037137920.ingest.de.sentry.io/4510451611205712",
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Enable Logs
+  enableLogs: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 1.0,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
+
+const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error: any, query) => {
+      Sentry.captureException(error, {
+        tags: {
+          type: "react-query-error",
+          queryKey: query.queryKey[0]?.toString() || "unknon",
+        },
+        extra: {
+          errorMessage: error.message,
+          statusCode: error.response?.status,
+          queryKey: query.queryKey,
+        },
+      });
+    },
+  }),
+  mutationCache: new MutationCache({
+    onError: (error: any) => {
+      // global error handler for all mutations
+      Sentry.captureException(error, {
+        tags: { type: "react-query-mutation-error" },
+        extra: {
+          errorMessage: error.message,
+          statusCode: error.response?.status,
+        },
+      });
+    },
+  }),
+});
+
+export default Sentry.wrap(function RootLayout() {
+  return (
+    <ClerkProvider 
+  publishableKey="pk_test_a2luZC1iaXNvbi05OC5jbGVyay5hY2NvdW50cy5kZXYk"
+  tokenCache={tokenCache}
+>
+      <QueryClientProvider client={queryClient}>
+        <StripeProvider publishableKey="pk_test_a2luZC1iaXNvbi05OC5jbGVyay5hY2NvdW50cy5kZXYk">
+          <Stack screenOptions={{ headerShown: false }} />
+        </StripeProvider>
+      </QueryClientProvider>
+    </ClerkProvider>
+  );
+});
